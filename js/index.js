4993b3d7bef6c41fa8433d54fae495a54f7a783b
Wei-Ming <barry800414@gmail.com>
2017-02-08 16:59:27 +0800

    very first component structure of this project by vue

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var heroList = new Vue({
+    el: '#hero-list',
+    data: {
+        heros: null,
+    },
+});
+
+
+var heroProfile = new Vue({
+    el: "#hero-profile",
+    data: {
+        profile: null,
+    },
+});
