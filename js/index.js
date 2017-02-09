96ddebe29e614a3f6bc233175dee2f86934c7b6f
Wei-Ming <barry800414@gmail.com>
2017-02-09 16:15:13 +0800

    send http request to get data

--- js/index.js
+++ js/index.js
@@ -3,12 +3,32 @@ var heroList = new Vue({
     data: {
         heros: null,
     },
+    created: function() {
+        this.$http.get("http://hahow-recruit.herokuapp.com/heroes").then(function(res) {
+            return res.json();
+        }).then(function(data){
+            this.heros = data;
+        });
+    },
+    methods: {
+        loadHero: function(hero_id){
+            heroProfile.loadHeroProfile(hero_id);
+        }
+    }
 });
-
 
 var heroProfile = new Vue({
     el: "#hero-profile",
     data: {
         profile: null,
     },
+    methods: {
+        loadHeroProfile: function(hero_id) {
+            this.$http.get(`http://hahow-recruit.herokuapp.com/heroes/${hero_id}/profile`).then(function(res) {
+                return res.json();
+            }).then(function(data){
+                this.profile = data;
+            });
+        }
+    }
 });
