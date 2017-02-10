f02acf48d258f0e5829679414743190440b25a62
Wei-Ming <barry800414@gmail.com>
2017-02-10 09:43:28 +0800

    update profile

--- js/index.js
+++ js/index.js
@@ -20,7 +20,9 @@ var heroList = new Vue({
 var heroProfile = new Vue({
     el: "#hero-profile",
     data: {
+        hero_id: 0,
         profile: null,
+        remain_points: 0,
     },
     methods: {
         loadHeroProfile: function(hero_id) {
@@ -28,6 +30,26 @@ var heroProfile = new Vue({
                 return res.json();
             }).then(function(data){
                 this.profile = data;
+                this.hero_id = hero_id;
+            });
+        },
+        addPoint: function(type) {
+            if(this.remain_points > 0){
+                this.profile[type] = this.profile[type] + 1;
+                this.remain_points = this.remain_points - 1;
+            }
+        },
+        minusPoint: function(type) {
+            if(this.profile[type] > 0){
+                this.profile[type] = this.profile[type] - 1;
+                this.remain_points = this.remain_points + 1;
+            }
+        },
+        updateProfile: function() {
+            this.$http.patch(`http://hahow-recruit.herokuapp.com/heroes/${this.hero_id}/profile`, this.profile).then(function(res) {
+                //update success
+            }, function(error){
+                //update failed
             });
         }
     }
